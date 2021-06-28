e0aaa30fb026850e182903e7a6ea0f35103ad3d1
homerking-092 <dudgns3603@naver.com>
2021-06-28 17:46:06 +0900

    2021 06 28

--- 11-javastatic/src/static1/StaticTest.java
+++ 11-javastatic/src/static1/StaticTest.java
@@ -12,6 +12,6 @@ public class StaticTest {
 		this.num1 = 5;
 		num2 += 1;
 		System.out.println("num1 변수는 수치가 변하지 않습니다: " + num1);
-		System.out.println("현재 가입자 수는" + num2 + "명 입니다");
+		System.out.println("현재 가입자 수는 " + num2 + "명 입니다");
 	}
 }
